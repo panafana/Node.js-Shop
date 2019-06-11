54ef69de6dd4ab95da9d4938c8af4021a8e68d0e
panafana <panafana@yahoo.gr>
2019-06-11 23:04:40 +0300

    adawd

--- routes/android.js
+++ routes/android.js
@@ -73,7 +73,7 @@ router.get('/products', function(req, res, next) {
     Product.find(function(err,docs){
         var products = [];
         for(var i=0;i<docs.length;i++){
-            products.push(docs.slice(i,i+chunkSize));
+            products.push(docs(i));
         }
         res.json(products);
     });
