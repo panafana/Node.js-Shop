a2c0b9831961e82068f44658bad5b185a5428727
panafana <panafana@yahoo.gr>
2019-06-12 22:07:51 +0300

    zxczxczxc

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(validator());
 app.use(session({
